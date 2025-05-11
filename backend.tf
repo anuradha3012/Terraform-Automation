526c68e2fcec97203f5d5679b23a329357f273f4
Anuradha <71402365+anuradha3012@users.noreply.github.com>
2025-05-11 17:39:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "myproject-developemt-terraform-bucket2"
+    bucket = "myproject-developemt-terraform-bucket2" //bucket to lock state file which we have manually created in step6
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
